9925270cc3d13f45bfcf5ff97de11e04a9704e42
shoaibtechs <p200147@pwr.nu.edu.pk>
2025-10-31 17:48:32 +0500

    Add views directory file

---
+++ 2_views_directory.js
@@ -0,0 +1,48 @@
+const express =  require("express");
+const app = express();
+
+const port = 8080;
+
+const path =  require("path");
+
+app.set("views", path.join(__dirname, "./views"));
+
+
+
+
+
+
+app.set("view engine", "ejs");
+
+
+app.listen(port, ()=>{
+
+
+    console.log(`The server is listening at port ${port}`);
+
+
+})
+
+
+app.get("/", (req, res)=>{
+
+    res.send("You contacted the Root Path ");
+
+
+})
+
+
+app.get("/home", (req, res)=>{
+
+    res.render("home");
+
+
+
+})
+
+
+
+
+
+
+
